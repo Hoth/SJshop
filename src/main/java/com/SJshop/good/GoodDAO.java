4d17a15e9f7cbef900ff483c16461da6b80dc47f
bit-user <bit-user@bit>
2016-11-15 11:47:04 +0900

    20161115_xml,DAO

--- src/main/java/com/SJshop/good/GoodDAO.java
+++ src/main/java/com/SJshop/good/GoodDAO.java
@@ -40,63 +40,42 @@ public class GoodDAO {
 		public static Good selectGood(int gNum) throws Exception {
 			
 			Good good = null;
-			//1.sql 쿼리를 실행하는 SqlSession 객체 생성해서 변수에 저장
-			//sqlMapper.openSession(true)
-			
 			SqlSession sqlSession = sqlMapper.openSession(true);
-			//2.1변수를 이용해서 Good.xml에 id 속성을 selectGood 이라고 작성한 쿼리 실행해서 조회결과를 ArrayList<Good> 변수에 저장
-			// (ArrayList)1변수.selectList("selectGood",gnum)
-			
 			ArrayList<Good>goodList =
 					(ArrayList) sqlSession.selectList("selectGood", gNum);
-			//3. 2의 변수가 null 이 아니고 2변수.size()가 0초과면
 			if(goodList!=null&&goodList.size()>0){
-			// 3.1 2변수의 index 0에 해당하는 객체를 변수 good에 저장
-			//  2변수.get(0)
 			good=goodList.get(0);
 			}
-			//4. 데이터 베이스 연결 종료
-			// 1의 변수.close()
-			sqlSession.close();
+					sqlSession.close();
 
-			return good;//물건 정보 리턴
+			return good;
 	  }
 
-	/*
-		전체 물건 조회
-		 */
-		
+	
 		public static ArrayList<Good> selectGoodList() throws Exception {
-			//1.sql 쿼리를 실행하는 SqlSession 객체 생성해서 변수에 저장
-			//sqlMapper.openSession(true)
 			SqlSession sqlSession = sqlMapper.openSession(true);
-			
 			ArrayList<Good>goodList = null;
-			//2.1변수를 이용해서 Good.xml에 id 속성을 selectGoodList 이라고 작성한 쿼리 실행해서 조회결과를 변수 goodList에 저장
-			// (ArrayList)1변수.selectList("selectGoodList")
 			goodList = (ArrayList)sqlSession.selectList("selectGoodList");
-			//4. 데이터 베이스 연결 종료
-			// 1의 변수.close()
 			sqlSession.close();
-			
-			return goodList;//전체 물건 정보 리턴
+			return goodList;
 	  }
 
-		/* 물건정보를 DB에 저장하는 메서드
-		 * 매개변수 good
-		 * -DB 에 저장하고 싶은 물건의 이름,가격,설명,이미지파일이름를 
-		 *  속성으로 저장하고있는 객체
-		 */
 		public static void insertGood(Good good) throws Exception {
-			//1.sql 쿼리를 실행하는 SqlSession 객체 생성해서 변수에 저장
-			//sqlMapper.openSession(true)
 			SqlSession sqlSession = sqlMapper.openSession(true);
-			//2.1변수를 이용해서 Good.xml에 id 속성을 insertGood 이라고 작성한 쿼리 실행
-			// 1변수.insert("insertGood",good)
 			sqlSession.insert("insertGood", good);
-			//4. 데이터 베이스 연결 종료
-			// 1의 변수.close()
 			sqlSession.close();
 
-		}//end method
+		}
+		
+		public static void deleteGood(Good good) throws Exception{
+			SqlSession sqlSession = sqlMapper.openSession(true);
+			sqlSession.insert("deleteGood",good.gNum);
+			sqlSession.close();
+		}
+		
+		public static void updateGood(Good good) throws Exception{
+			SqlSession sqlSession = sqlMapper.openSession(true);
+			sqlSession.insert("updateGood",good.gNum);
+			sqlSession.close();
+		}
 }
--- src/main/java/com/SJshop/member/MemberDAO.java
+++ src/main/java/com/SJshop/member/MemberDAO.java
@@ -30,44 +30,33 @@ public class MemberDAO {
 	public static Member selectMember(int mNum) throws Exception {
 		
 		Member member= null;
-		//1.sql 쿼리를 실행하는 SqlSession 객체 생성해서 변수에 저장
-		//sqlMapper.openSession(true)
-		
-		SqlSession sqlSession = sqlMapper.openSession(true);
-		//2.1변수를 이용해서 Good.xml에 id 속성을 selectGood 이라고 작성한 쿼리 실행해서 조회결과를 ArrayList<Good> 변수에 저장
-		// (ArrayList)1변수.selectList("selectGood",gnum)
-		
+		SqlSession sqlSession = sqlMapper.openSession(true);	
 		ArrayList<Member>memList =
 				(ArrayList) sqlSession.selectList("selectMember", mNum);
-		//3. 2의 변수가 null 이 아니고 2변수.size()가 0초과면
 		if(memList!=null&&memList.size()>0){
-		// 3.1 2변수의 index 0에 해당하는 객체를 변수 good에 저장
-		//  2변수.get(0)
 		member=memList.get(0);
 		}
-		//4. 데이터 베이스 연결 종료
-		// 1의 변수.close()
 		sqlSession.close();
 
-		return member;//물건 정보 리턴
+		return member;
   }
 	
 	public static void insertMember(Member member) throws Exception {
-		//1.sql 쿼리를 실행하는 SqlSession 객체 생성해서 변수에 저장
-		//sqlMapper.openSession(true)
 		SqlSession sqlSession = sqlMapper.openSession(true);
-		//2.1변수를 이용해서 Good.xml에 id 속성을 insertGood 이라고 작성한 쿼리 실행
-		// 1변수.insert("insertGood",good)
 		sqlSession.insert("insertMember", member);
-		//4. 데이터 베이스 연결 종료
-		// 1의 변수.close()
 		sqlSession.close();
 
-	}//end method
+	}
 	
-	public static void deleteMember(int mNum) throws Exception{
+	public static void deleteMember(Member member) throws Exception{
 		SqlSession sqlSession = sqlMapper.openSession(true);
-		sqlSession.insert("deleteMember",mNum);
+		sqlSession.insert("deleteMember",member.mNum);
+		sqlSession.close();
+	}
+	
+	public static void updateGood(Member member) throws Exception{
+		SqlSession sqlSession = sqlMapper.openSession(true);
+		sqlSession.insert("updateGood",member.mNum);
 		sqlSession.close();
 	}
 
